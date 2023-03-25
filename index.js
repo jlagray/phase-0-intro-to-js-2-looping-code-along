8412a965f8266fac2b85003bda96c0365baaba5a
Jamie Grayson <126696913+jlagray@users.noreply.github.com>
2023-03-25 19:59:01 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// Code your solutions in this file!
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, value) {
+    for (let i = 0; i < names.length; i++) {
+     messageNames.push([`Thank you, ${names[i]},for the wonderful ${value} gifts!`])
+    }
+     return messageNames
+    
+}
+
+function countDown(number) {
+    while (number >= 0) {
+    console.log (number)
+    number --
+    }
+}
